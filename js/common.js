2d8cbb51ebc7c402a222c7ddc58b75b58436ca21
김성은 <kse012050@naver.com>
2021-04-28 02:46:19 +0900

    main slider를 만들었다

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var swiper = new Swiper('.mainSliderArea .swiper-container', {
+        navigation: {
+          nextEl: '.mainSliderArea .swiper-button-next',
+          prevEl: '.mainSliderArea .swiper-button-prev',
+        },
+    });
+});
+
