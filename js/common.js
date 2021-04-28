da3db0abb796a9c8e45f1b3709bf523dc6a50ef8
김성은 <kse012050@naver.com>
2021-04-28 09:48:51 +0900

    mobile 메뉴를 만들었다

--- js/common.js
+++ js/common.js
@@ -5,5 +5,29 @@ $(document).ready(function(){
           prevEl: '.mainSliderArea .swiper-button-prev',
         },
     });
+
+    mainScroll();
+    mobileMenuClick();
 });
 
+
+function mainScroll(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 0){
+            $('header').addClass('on');
+        }else{
+            $('header').removeClass('on');
+        }
+    });
+}
+
+
+function mobileMenuClick(){
+    $('header .menuBtn').click(function(){
+        $('header nav').toggleClass('active');
+    })
+    
+    $('ul.menuArea > li > a').click(function(){
+        $(this).next().slideToggle();
+    });
+}
